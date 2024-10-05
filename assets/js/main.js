12273211afe3054fbceaada4143387d79f673611
amirulkanak <coder.kanak@gmail.com>
2024-10-05 10:07:44 +0600

    review 1

--- assets/js/main.js
+++ assets/js/main.js
@@ -136,26 +136,6 @@ const addRemoveActiveClass = (categoryBtn) => {
   categoryBtn.classList.add('bg-red-500', 'text-white');
 };
 
-const video12 = {
-  category_id: '1001',
-  video_id: 'aaaa',
-  thumbnail: 'https://i.ibb.co/L1b6xSq/shape.jpg',
-  title: 'Shape of You',
-  authors: [
-    {
-      profile_picture: 'https://i.ibb.co/D9wWRM6/olivia.jpg',
-      profile_name: 'Olivia Mitchell',
-      verified: '',
-    },
-  ],
-  others: {
-    views: '100K',
-    posted_date: '16278',
-  },
-  description:
-    "Dive into the rhythm of 'Shape of You,' a captivating track that blends pop sensibilities with vibrant beats. Created by Olivia Mitchell, this song has already gained 100K views since its release. With its infectious melody and heartfelt lyrics, 'Shape of You' is perfect for fans looking for an uplifting musical experience. Let the music take over as Olivia's vocal prowess and unique style create a memorable listening journey.",
-};
-
 // Display videos card on the page
 
 const displayVideosCard = (videos) => {
